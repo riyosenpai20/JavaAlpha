6ba2c725eeaada618af4389da43e13d76e50e335
dwiga <dwi.hyuga@gmail.com>
2017-05-11 19:47:44 +0700

    trying class puppyAge

--- src/javaalpha/JavaAlpha.java
+++ src/javaalpha/JavaAlpha.java
@@ -5,21 +5,30 @@ package javaalpha;
  * @author avos
  */
 
-class FreshJuice{
-    enum FreshJuiceSize {Small, Medium, Large}
-    FreshJuiceSize size;
-}
 
 public class JavaAlpha {
-
+    int puppyAge;
+    
+    public JavaAlpha (String name){
+        System.out.println("The name is : " + name);
+    }
+    public void setAge(int age){
+        puppyAge = age;
+    }
+    public int getAge(){
+        System.out.println("My Puppy's age : " + puppyAge);
+        return puppyAge;
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        FreshJuice juice = new FreshJuice();
-        juice.size = FreshJuice.FreshJuiceSize.Medium;
-        System.out.println("Size : " + juice.size);
+        JavaAlpha myPuppy = new JavaAlpha("gugy");
+        myPuppy.setAge(7);
+        myPuppy.getAge();
+        System.out.println("Variable value : " + myPuppy.puppyAge);
     }
     
 }

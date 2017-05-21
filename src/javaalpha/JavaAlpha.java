e2b6eb344d079ab9beedf7c9e4d69e55b8f22577
dwiga <dwi.hyuga@gmail.com>
2017-05-21 18:10:42 +0700

    showing date

--- src/javaalpha/JavaAlpha.java
+++ src/javaalpha/JavaAlpha.java
@@ -1,34 +1,22 @@
 package javaalpha;
 
-import java.sql.*;
+import java.util.Date;
+import java.text.*;
 /**
  *
  * @author avos
  */
 
 public class JavaAlpha {
-    int puppyAge;
-    
-    public JavaAlpha (String name){
-        System.out.println("The name is : " + name);
-    }
-    public void setAge(int age){
-        puppyAge = age;
-    }
-    public int getAge(){
-        System.out.println("My Puppy's age : " + puppyAge);
-        return puppyAge;
-    }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        JavaAlpha myPuppy = new JavaAlpha("gugy");
-        myPuppy.setAge(7);
-        myPuppy.getAge();
-        System.out.println("Variable value : " + myPuppy.puppyAge);
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("Y-m-d");
+        System.out.println(sdf.format(date));
     }
     
 }
